dd3b6c1fccdf02a728160093fd217e19ec87ed67
Walter <walterwong1110@outlook.com>
2024-07-12 02:49:55 +0800

    修改package名称

--- services/user.go
+++ services/user.go
@@ -1,17 +1,78 @@
-package services
+package service
 
-import repository "github.com/weitien/admin/repositories"
+import (
+	"context"
+	"time"
 
-type UserService struct {
-	Repository *repository.userRepositoryImpl
+	"github.com/weitien/admin/models"
+	"github.com/weitien/admin/repository"
+)
+
+type UserService interface {
+	CreateUser(ctx context.Context, user *models.User) error
 }
 
-func NewUserService() *UserService {
-	return &UserService{
-		Repository: repository.NewUserRepository(),
+type userServiceImpl struct {
+	repository        repository.UserRepository
+	accountRepository repository.AccountRepository
+}
+
+func NewUserService() UserService {
+	return &userServiceImpl{
+		repository:        repository.NewUserRepository(),
+		accountRepository: repository.NewAccountRepository(),
 	}
 }
 
-func (s *UserService) CreateUser() {
+func (s *userServiceImpl) CreateUser(ctx context.Context, user *models.User) (err error) {
+	tx := repository.DB.Begin()
+	if tx.Error != nil {
+		return tx.Error
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		} else if err != nil {
+			tx.Rollback()
+		} else {
+			err = tx.Commit().Error
+		}
+	}()
 
+	// 创建用户
+	if err = s.repository.CreateUser(ctx, user); err != nil {
+		return err
+	}
+
+	// 创建默认账户
+	if err = s.accountRepository.NewAccounts(ctx, s.getDefaultAccounts(user)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *userServiceImpl) getDefaultAccounts(u *models.User) []*models.Account {
+	// Username Account
+	acc := models.Account{
+		ID:         1,
+		UserID:     u.ID,
+		Identifier: u.Name,
+		Password:   "123",
+		Type:       models.AccountTypeUsername,
+		Status:     models.AccountActivity,
+		CreateTime: time.Now().UnixMilli(),
+	}
+	// Email Account
+	emailAcc := acc
+	emailAcc.ID = 2
+	emailAcc.Identifier = u.Email
+	emailAcc.Type = models.AccountTypeEmail
+	// Mobile Account
+	mobileAcc := acc
+	mobileAcc.ID = 3
+	mobileAcc.Identifier = u.Mobile
+	mobileAcc.Type = models.AccountTypeMobile
+
+	return []*models.Account{&acc, &emailAcc, &mobileAcc}
 }
